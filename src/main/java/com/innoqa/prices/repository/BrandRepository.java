12d1d692565fcea2517de910dc90b7b83f9c63b7
cfernandezcairo <cfernandezcairo@gmail.com>
2023-08-26 09:43:34 -0400

    Add interfaces repository para que funcione poblar el almacen a partir de los modelos

---
+++ src/main/java/com/innoqa/prices/repository/BrandRepository.java
@@ -0,0 +1,11 @@
+package com.innoqa.prices.repository;
+
+import com.innoqa.prices.model.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface BrandRepository extends JpaRepository<Brand, Long>{
+
+}
